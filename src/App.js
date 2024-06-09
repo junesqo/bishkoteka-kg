d1656d54f9133ca6178bb26638ee4f1ba0215b36
Nurel Sovetbekov <62104475+junesqo@users.noreply.github.com>
2024-06-09 23:51:57 +0600

    something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/bishkoteka.apk'; // Укажите путь к вашему файлу
+    link.download = 'bishkoteka.apk';
+    link.click();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="title">БИШКОТЕКА</h1>
+        <p className="description">
+          Афиша мероприятий в Бишкеке. Нажми на кнопку "Скачать" ниже и начни искать мероприятия уже сейчас!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="download-button" onClick={handleDownload}>Скачать</button>
       </header>
+      <section className="photos">
+        <img src="/path-to-photo1.jpg" alt="Фото 1" className="photo" />
+        <img src="/path-to-photo2.jpg" alt="Фото 2" className="photo" />
+        <img src="/path-to-photo3.jpg" alt="Фото 3" className="photo" />
+      </section>
     </div>
   );
 }
